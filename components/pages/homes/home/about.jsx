8538b1bf56b3835421a606cfec79955a9c60fa49
Jerry <jerrypaulsam@shopdibz.com>
2024-06-04 20:01:41 +0530

    fourth commit

--- components/pages/homes/home/about.jsx
+++ components/pages/homes/home/about.jsx
@@ -4,7 +4,9 @@ import shape3 from "../../../../public/assets/img/shape/about-2.png";
 import shape4 from "../../../../public/assets/img/shape/about-2-dark.png";
 import image1 from "../../../../public/assets/img/about/abo-1.jpg";
 import image2 from "../../../../public/assets/img/about/abo-2.jpg";
-import avatar from "../../../../public/assets/img/avatar/secretary.jpeg"; 
+import avatar from "../../../../public/assets/img/avatar/secretary.jpeg";
+import avatar2 from "../../../../public/assets/img/avatar/rajeeve.jpg";
+
 import Count from "../../common/count";
 import Link from "next/link";
 
@@ -15,8 +17,10 @@ const About = () => {
         subtitle: 'About Khadi Board',
         title: '',
         description: 'Kerala Khadi and Village Industries Board is a statutory body constituted by the Act 9 of 1957, vested with the responsibility of organizing and promoting Khadi and Village Industries in the State. The Board implements the programmes through co-operative, registered institutions, individuals and departmental units by imparting/ availing assistance from Government of Kerala, Khadi Commission and Nationalized Banks.',
-        author_name: 'Shri. Dr. K A Ratheesh',
+        author_name: 'Dr. K A Ratheesh',
         author_sub: 'Secretary',
+        minister_name: 'Shri. P Rajeeve',
+        minister_sub: 'Chairman',
         btn_title: 'More',
         btn_link: '/en/about-us'
     }
@@ -27,11 +31,11 @@ const About = () => {
                     <div className="col-xl-6 col-lg-6 lg-mb-30">
                         <div className="about__one-left">
                             <div className="about__one-left-image">
-                                <img className="one" src={image1.src} alt="about-image" style={{borderRadius: "5px"}} />
-                                <img className="two" src={image2.src} alt="about-image" style={{borderRadius: "5px"}} />
+                                <img className="one" src={image1.src} alt="about-image" style={{ borderRadius: "5px" }} />
+                                <img className="two" src={image2.src} alt="about-image" style={{ borderRadius: "5px" }} />
                             </div>
                             <div className="about__one-left-experience">
-                                <h1><Count number={aboutData?.count}/>+</h1>
+                                <h1><Count number={aboutData?.count} />+</h1>
                                 <h6>{aboutData?.exp}</h6>
                             </div>
                         </div>
@@ -43,10 +47,17 @@ const About = () => {
                                 <h2>{aboutData?.title}</h2>
                                 <p>{aboutData?.description}</p>
                             </div>
-                            <div className="about__one-right-btn">
-                                <div>
-                                    <Link className="btn-one" href={aboutData?.btn_link}>{aboutData?.btn_title}<i className="far fa-chevron-double-right"></i></Link>
+                            <div className="about__one-right-btn" style={{display: "flex", justifyContent: "center"}}>
+                                <div className="about__one-right-btn-author">
+                                    <div className="about__one-right-btn-author-avatar">
+                                        <img src={avatar2.src} alt={`Khadi Board - ${aboutData?.minister_name}`} />
+                                    </div>
+                                    <div className="about__one-right-btn-author-name">
+                                        <span className="text-one">{aboutData?.minister_name}</span>
+                                        <h6>{aboutData?.minister_sub}</h6>
+                                    </div>
                                 </div>
+
                                 <div className="about__one-right-btn-author">
                                     <div className="about__one-right-btn-author-avatar">
                                         <img src={avatar.src} alt={`Khadi Board - ${aboutData?.author_name}`} />
@@ -56,6 +67,10 @@ const About = () => {
                                         <h6>{aboutData?.author_sub}</h6>
                                     </div>
                                 </div>
+                            </div>
+                            <div style={{display: "flex", justifyContent: "center"}}>
+                                <Link className="btn-one mt-30"
+                                 href={aboutData?.btn_link}>{aboutData?.btn_title}<i className="far fa-chevron-double-right"></i></Link>
                             </div>
                             {/* <div className="about__one-right-bottom">
                                 <div className="about__one-right-bottom-list">
