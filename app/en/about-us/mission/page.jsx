5fd3c6a803ecd55db3106193cc78c181bde0d13d
Jerry <jerrypaulsam@shopdibz.com>
2024-06-07 10:22:05 +0530

    seventh commit

--- app/en/about-us/mission/page.jsx
+++ app/en/about-us/mission/page.jsx
@@ -1,10 +1,8 @@
 import SEO from "@/components/data/seo";
 import HeaderOne from "@/components/layout/headers/header-one";
 import BreadCrumb from "../../../../components/pages/common/breadcrumb";
-import AboutMain from "../../../../components/pages/about/about";
 import FooterOne from "@/components/layout/footers/footer-one";
 import ScrollToTop from "../../../../components/pages/common/scroll/scroll-to-top";
-import WhoWeAre from "../../../../components/pages/about/who-we-are";
 import History from "../../../../components/pages/about/history";
 
 const Mission = () => {
---
+++ components/pages/gallery/services.jsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import photoGalleryData from '@/components/data/photo-gallery';
+
+const PhotoAlbums = () => {
+    return (
+        <>
+            <div className="services__page section-padding-two">
+                <div className="container">
+                    <div className="row">
+                        {photoGalleryData?.map((data, id) => (
+                            <div className="col-xl-4 col-lg-4 col-md-6 mt-25" key={id}>
+                                <Link href={`/en/gallery/${data.id}`}>
+                                    <div className="services__three-item page">
+                                        <img src={data.image.src} alt="Kerala Khadi Photo Gallery" />
+                                        <div className="services__three-item-content page">
+                                            <h4>{data.title}</h4>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default PhotoAlbums;
