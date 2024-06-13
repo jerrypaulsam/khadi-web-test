7cd0d4fde34d0b70c9f877580c0e480a2c141cd0
Jerry <jerrypaulsam@shopdibz.com>
2024-06-13 18:57:33 +0530

    13/6 commit

--- components/pages/homes/home/NewsTicker.jsx
+++ components/pages/homes/home/NewsTicker.jsx
@@ -10,12 +10,11 @@ const NewsTicker = ({ isMalayalam }) => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const accessToken = '92464c05f949ae555b6b11da26e315ba'; // Replace with your access token
-        const userId = 'khadikerala'; // Replace with your Instagram user ID
+        const accessToken = 'IGQWROSExGN2xxa2pKSExMLXJ5SXdEaXZAHM051QkNNVHoxY2E4TktYMEJiOGRTaXdrVzZAxZAmhsVWJZAektLYVVaNXR1OWVXX0VVVzhyZAXVJZATdzZAlZAybTNCcm1LdEplNWxfbE1tSTdac1d1TmpOSnByX2s1cU91ZA2cZD'; // Replace with your access token
+        const userId = '54819600064'; // Replace with your Instagram user ID
         const fetchInstagramPhotos = async () => {
             try {
                 const response = await fetch(`https://graph.instagram.com/${userId}/media?fields=id,media_type,media_url,caption,permalink&access_token=${accessToken}`);
-                console.log(response.status);
                 const data = await response.json();
                 setPhotos(data.data);
             } catch (error) {
@@ -27,6 +26,8 @@ const NewsTicker = ({ isMalayalam }) => {
 
         fetchInstagramPhotos();
     }, [photos, loading]);
+
+    console.log(photos);
 
     return (
         <div className="news-ticker-container experience__area" style={{ display: 'flex' }}>
@@ -57,18 +58,18 @@ const NewsTicker = ({ isMalayalam }) => {
             </div>
 
             <div className="news-ticker-right-side" >
-                <h6>Instagram Photos</h6>
-                {photos.length === 0 ? <p style={{textAlign: "center", fontSize: "14px"}} >No photos found</p> : <p></p>}
+                <h6>{isMalayalam ? "ഇൻസ്റ്റാഗ്രാം ഫോട്ടോകൾ" : "Instagram Photos"}</h6>
+                {(photos != undefined && photos != null) || photos?.length === 0 ? <p style={{textAlign: "center", fontSize: "14px"}} >{isMalayalam ? "ഫോട്ടോകളൊന്നും കണ്ടെത്തിയില്ല" : "No photos found"}</p> : <p></p>}
                 {loading ? (
                     <div className="instagram-loader-container">
                         <div className="instagram-loader"></div>
                     </div>
                 ) : (
-                    <div id="instafeed">
-                        {photos.length === 0 ? (
+                    <div id="instafeed"> 
+                        {(photos != undefined && photos != null) || photos?.length === 0  ? (
                             <p style={{textAlign: "right"}} ></p>
                         ) : (
-                            photos.map(photo => (
+                            photos?.map(photo => (
                                 <a key={photo.id} href={photo.permalink} target="_blank" rel="noopener noreferrer">
                                     <img src={photo.media_url} alt={photo.caption} />
                                 </a>
